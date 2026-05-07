0b88b3f1cbfe8cb50c10a1cf039fa26e90eb276e
MamboKNIGHT2 <127346189+MamboKNIGHT2@users.noreply.github.com>
2026-05-07 15:04:30 -0500

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,12 +1,12 @@
-// ⚠️ Reemplaza con los datos de tu proyecto Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSy...",
-    authDomain: "tudominio.firebaseapp.com",
-    databaseURL: "https://tudominio-default-rtdb.firebaseio.com",
-    projectId: "tuprojectid",
-    storageBucket: "tuprojectid.appspot.com",
-    messagingSenderId: "123456",
-    appId: "1:123456:web:abc..."
+  apiKey: "AIzaSyAkG4IzT4eIPVh1hidApZFsBd7hYqws0WQ",
+  authDomain: "mambokaraoke-86864.firebaseapp.com",
+  databaseURL: "https://mambokaraoke-86864-default-rtdb.firebaseio.com",
+  projectId: "mambokaraoke-86864",
+  storageBucket: "mambokaraoke-86864.firebasestorage.app",
+  messagingSenderId: "276665091698",
+  appId: "1:276665091698:web:b45930fc18592f7a3924eb",
+  measurementId: "G-SYDZXW642F"
 };
 
 firebase.initializeApp(firebaseConfig);
