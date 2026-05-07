a3a8eaf357ee6ffdd0b9ea797014663fb173628c
MamboKNIGHT2 <127346189+MamboKNIGHT2@users.noreply.github.com>
2026-05-07 15:17:54 -0500

    Update search.js

--- js/search.js
+++ js/search.js
@@ -1,5 +1,5 @@
 // Búsqueda usando YouTube Data API v3 (requiere clave de API gratuita)
-const YOUTUBE_API_KEY = 'AIzaSy...TU_CLAVE'; // ← CAMBIA ESTO
+const YOUTUBE_API_KEY = 'AIzaSyD-NKQi-0Si0dDy8z5-w9F563iAIyi27R0';
 
 async function searchYouTube(query) {
     const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=12&q=${encodeURIComponent(query)}&key=${YOUTUBE_API_KEY}`;
